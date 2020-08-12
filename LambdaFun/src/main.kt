8f44899f095f1e472b9d74bff31952c437246749
EVGENII <watersmark@mail.ru>
2020-08-12 13:05:43 +0300

    Lambda function new

--- LambdaFun/src/main.kt
+++ LambdaFun/src/main.kt
@@ -1,18 +1,14 @@
+fun convert(x: Double, converter: (Double) -> Double): Double{
+
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+
+}
+
+
 fun main() {
-
-    // Работаем с лямбда выражениями
-
-    val temp1: (Int, Int) -> Int =  {x: Int, y: Int -> x + y}
-    println(temp1(6 , 3))
-
-    val temp2: (Int, Int) -> Unit = {x, y -> x + y}
-    println(temp2(3, 4))
-
-    val temp3: () -> String = {"This is horosho"}
-    println(temp3())
-
-    val temp4: (Int) -> Double = {it + 3.4}
-    println(temp4(1))
-
-    
+    convert(20.toDouble(), {println("GG")
+        it * 1.8 + 32
+                             })
 }
