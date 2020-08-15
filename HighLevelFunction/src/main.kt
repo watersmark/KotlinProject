ba3ace5ff764dd124883665c6bddd7057cc1a8e3
EVGENII <watersmark@mail.ru>
2020-08-15 13:44:20 +0300

    forEach

--- HighLevelFunction/src/main.kt
+++ HighLevelFunction/src/main.kt
@@ -10,12 +10,7 @@ data class temp(val a: Int) {
 fun main() {
 
     val list_test: List<Int> = mutableListOf(1, 2, 3, 12, 7, 5)
-    val temp1 = list_test.filter { it > 3 }.map { it  }
-
-    
-    for(i in temp1){
-        println(i)
-    }
+    val new_1 = list_test.filter{it > 3}.forEach{ println(it)}
 
 }
 
